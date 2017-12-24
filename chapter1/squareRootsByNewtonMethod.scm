a363cb3709d9e49815696ac8ce04a66bd019948e
openx2 <xiaoyanghaitao@outlook.com>
2017-12-25 00:22:22 +0800

    Use interal definitions for sqrt

--- chapter1/squareRootsByNewtonMethod.scm
+++ chapter1/squareRootsByNewtonMethod.scm
@@ -1,20 +1,20 @@
 (begin
   (define ~ exact->inexact)
-  (define (average a b)
-    (/ (+ a b) 2))
-  (define (square x) (* x x))
-  (define (improve guess x)
-    (average guess (/ x guess)))
-  (define (good-enough? guess x)
-    ;(< (abs (- (square guess) x)) 0.001)) ;orginal test
-    (< (/ (abs (- (improve guess x) guess)) guess) 1/100)) ;small fraction test
-  (define (sqrt-iter guess x)
-    (if (good-enough? guess x)
-      (~ guess)
-      ;guess
-      (sqrt-iter (improve guess x) x)))
   (define (my_sqrt x)
-    (sqrt-iter 1 x))
+    (define (average a b)
+      (/ (+ a b) 2))
+    (define (square a) (* a a))
+    (define (improve guess)
+      (average guess (/ x guess)))
+    (define (good-enough? guess)
+      ;(< (abs (- (square guess) x)) 0.001)) ;orginal test
+      (< (/ (abs (- (improve guess) guess)) guess) 1/100)) ;small fraction test
+    (define (sqrt-iter guess)
+      (if (good-enough? guess)
+        (~ guess)
+        ;guess
+        (sqrt-iter (improve guess))))
+    (sqrt-iter 1))
   (display (my_sqrt 0.000001)) ;exercise1.7 small number "Not precise"
   (newline)
   (display (my_sqrt 1000000)) ;exercise1.7 large number "TOO LONG TIME!!!"
