cb35221fb0fb197a8a889a7914a1f3ac0413a148
openx2 <xiaoyanghaitao@outlook.com>
2017-12-25 18:16:30 -0800

    chapter1.2.1 example

---
+++ chapter1/factorial.scm
@@ -0,0 +1,18 @@
+;linear recursive process
+;(define (factorial n)
+;  (if (= n 1)
+;    1
+;    (* n (factorial (- n 1)))))
+
+;linear recursive process
+(define (factorial n)
+  (define (iter product counter)
+    (if (> counter n)
+      product
+      (iter (* product counter)
+		      (+ counter 1))))
+    (iter 1 1))
+
+(begin
+  (display (factorial 5))
+  (newline))
